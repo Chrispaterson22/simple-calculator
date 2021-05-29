337ade5a10fe3a8c00cfe1587fd2c394c8048c75
Chris Paterson <chrispaterson22@gmail.com>
2021-05-29 15:59:08 +0100

    specs set up

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,3 +1,14 @@
 describe('calculator.js test suite', () => {
-	// TODO: Specs!
+	it('should add numbers to total', () => {
+		// TODO: Expectations
+	})
+	it('should subtract numbers from total', () => {
+		// TODO: Expectations
+	})
+	it('should multiply total by number', () => {
+		// TODO: Expectations
+	})
+	it('should divide total by number', () => {
+		// TODO: Expectations
+	})
 })
