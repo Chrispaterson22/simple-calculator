4a8fd84ec36f4254853c28371f0551dc69de2777
Chris Paterson <chrispaterson22@gmail.com>
2021-05-29 21:07:51 +0100

    Matchers section complete

--- calculator.spec.js
+++ calculator.spec.js
@@ -27,6 +27,8 @@ describe('calculator.js test suite', () => {
 		expect(calculator.total).toBe(100)
 	})
 
+	// Examples of different Matchers in Jasmine
+
 	// toBe
 	// toBeFalsy
 	it('should initialize the total', () => {
@@ -38,12 +40,83 @@ describe('calculator.js test suite', () => {
 
 	// toEqual
 	// toBeTruthy
+	// toContain
+	// Custom Matcher
 	it('can be instantiated', () => {
+		jasmine.addMatchers(customMatchers)
 		const calculator = new Calculator()
 		const calculator2 = new Calculator()
 
+		expect(calculator).toBeCalculator()
+		expect(2).not.toBeCalculator()
 		expect(calculator).toBeTruthy()
 		expect(calculator2).toBeTruthy()
 		expect(calculator).toEqual(calculator2)
+		expect(calculator.constructor.name).toContain('ator') // 'Calculator'
+	})
+
+	// use of: not
+	it('instantiates unique object', () => {
+		const calculator1 = new Calculator()
+		const calculator2 = new Calculator()
+
+		expect(calculator1).not.toBe(calculator2)
+	})
+
+	// toBeDefined, toBeUndefined
+	it('has common operations', () => {
+		const calculator = new Calculator()
+
+		expect(calculator.add).toBeDefined()
+		expect(calculator.subtract).toBeDefined()
+		expect(calculator.multiply).toBeDefined()
+		expect(calculator.divide).toBeDefined()
+	})
+
+	// toBeNull
+	it('can overwrite total', () => {
+		const calculator = new Calculator()
+
+		calculator.total = null
+		expect(calculator.total).toBeNull()
+	})
+
+	// toBeNaN
+	it('does not handle NaN', () => {
+		const calculator = new Calculator()
+
+		calculator.total = 20
+		calculator.multiply('a')
+
+		expect(calculator.total).toBeNaN()
+	})
+
+	// toThrow
+	// toThrowError
+	it('handles divide by zero', () => {
+		const calculator = new Calculator()
+
+		expect(() => calculator.divide(0)).toThrow()
+		expect(() => calculator.divide(0)).toThrowError(Error)
+		expect(() => calculator.divide(0)).toThrowError(
+			Error,
+			'Cannot divide by zero'
+		)
+	})
+
+	// toMatch
+	// match anything! with jasmine.anything() only fails for null and undefined
+	// 3rd party toBeNumber
+	it('returns the total', () => {
+		const calculator = new Calculator()
+		calculator.total = 50
+
+		expect(calculator.add(20)).toBe(70)
+		expect(calculator.total).toMatch(/-?\d+/)
+		expect(calculator.total).toBeNumber()
+
+		// asymmetric matchers
+		// not equal in each side!
+		expect(calculator.total).toEqual(jasmine.anything())
 	})
 })
