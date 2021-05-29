6a959cb2fec17e897219055c1753205a40c20419
Chris Paterson <chrispaterson22@gmail.com>
2021-05-29 16:18:19 +0100

    expectations done, all passing

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,14 +1,29 @@
 describe('calculator.js test suite', () => {
 	it('should add numbers to total', () => {
-		// TODO: Expectations
+		const calculator = new Calculator()
+		calculator.add(5)
+		// expect to be 5
+		expect(calculator.total).toBe(5)
 	})
 	it('should subtract numbers from total', () => {
-		// TODO: Expectations
+		const calculator = new Calculator()
+		calculator.total = 30
+		calculator.subtract(5)
+
+		expect(calculator.total).toBe(25)
 	})
 	it('should multiply total by number', () => {
-		// TODO: Expectations
+		const calculator = new Calculator()
+		calculator.total = 100
+		calculator.multiply(2)
+
+		expect(calculator.total).toBe(200)
 	})
 	it('should divide total by number', () => {
-		// TODO: Expectations
+		const calculator = new Calculator()
+		calculator.total = 200
+		calculator.divide(2)
+
+		expect(calculator.total).toBe(100)
 	})
 })
